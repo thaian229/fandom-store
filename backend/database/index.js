466abc8bce5ccd624037000883c8fd7030faf6aa
mHoang99 <vmhoang1999@gmail.com>
2020-04-26 21:44:15 +0700

    new

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,51 +1,20 @@
-const { Pool } = require('pg')
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "bvtnerlu",
-    host: "satao.db.elephantsql.com",
-    database: "bvtnerlu",
-    password: "Oq5jbXifegwkVVoU951jegUofOtDU7Yx",
-    port: 5432
-})
+  user: "bvtnerlu",
+  host: "satao.db.elephantsql.com",
+  database: "bvtnerlu",
+  password: "Oq5jbXifegwkVVoU951jegUofOtDU7Yx",
+  port: 5432,
+});
 
-pool.connect()
-    .then(() => console.log("Connected Successfully"))
-    .catch((e) => {
-        console.log(e);
-    })
-
+pool
+  .connect()
+  .then(() => console.log("Connected Successfully"))
+  .catch((e) => {
+    console.log(e);
+  });
 
 module.exports = {
-    query: (text, params, callback) => {
-        const start = Date.now()
-        return pool.query(text, params, (err, res) => {
-            const duration = Date.now() - start
-            console.log('executed query', { text, duration, rows: res.rowCount })
-            callback(err, res)
-        })
-    },
-    getClient: (callback) => {
-        pool.connect((err, client, done) => {
-            const query = client.query
-            // monkey patch the query method to keep track of the last query executed
-            client.query = (...args) => {
-                client.lastQuery = args
-                return query.apply(client, args)
-            }
-            // set a timeout of 5 seconds, after which we will log this client's last query
-            const timeout = setTimeout(() => {
-                console.error('A client has been checked out for more than 5 seconds!')
-                console.error(`The last executed query on this client was: ${client.lastQuery}`)
-            }, 5000)
-            const release = (err) => {
-                // call the actual 'done' method, returning this client to the pool
-                done(err)
-                // clear our timeout
-                clearTimeout(timeout)
-                // set the query method back to its old un-monkey-patched version
-                client.query = query
-            }
-            callback(err, client, release)
-        })
-    }
-}
+  query: (text, params) => pool.query(text, params)
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,11 @@
-const bodyParser = require('body-parser');
 const postRouter = require('./users/posts.router');
 const userRouter = require('./users/users.router');
 const uploadRouter = require('./uploads/uploads.router');
 
 module.exports = app => {
-    app.use(bodyParser.json());
     app.use('/users', userRouter);
     app.use('/posts', postRouter);
     app.use('/upload', uploadRouter);
-    app.get('/', (req, res) => res.render('./index'));
 }
 
 
