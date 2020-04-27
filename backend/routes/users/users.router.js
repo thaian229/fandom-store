4767a8916fd05aecf4c16d463f16362099e7eb09
Nguyễn Thái An <thaian229@gmail.com>
2020-04-27 11:29:59 +0700

    Add users/register (tested)

--- backend/routes/users/users.router.js
+++ backend/routes/users/users.router.js
@@ -5,9 +5,65 @@ const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+")
 
 const userRouter = express.Router();
 
-userRouter.get("/register", async (req, res) => {});
+userRouter.post("/register", async (req, res) => {
+    // get email and password from req.body
+    const { email, password } = req.body;
+    console.log(email)
+    console.log(password)
 
-userRouter.get("/login", async (req, res) => {});
+    // validate email
+    if (!email || !emailRegex.test(email)) {
+        res.status(400).json({
+            success: false,
+            message: 'Invalid email address',
+        });
+    } else if (!password || password.length < 6) { // validate password
+        res.status(400).json({
+            success: false,
+            message: 'Password must be at least 6 characters',
+        });
+    } else {
+        // check weather email exist or not
+        try {
+            const { rows } = await db.query(`SELECT email FROM accounts WHERE email = $1::text LIMIT 1`, [email])
+            if (rows[0]) {
+                res.status(400).json({
+                    success: false,
+                    message: 'This email has been used',
+                });
+            } else {
+                // hash password
+                const hashPassword = bcryptjs.hashSync(password, 10);
+
+                // save to database
+                try {
+                    await db.query(`INSERT INTO accounts (email, password) VALUES ($1::text, $2::text)`, [email, hashPassword]);
+                    const { rows } = await db.query(`SELECT id FROM accounts WHERE email = $1::text LIMIT 1`, [email])
+                    res.status(201).json({
+                        success: true,
+                        data: {
+                            id: rows[0].id,
+                            email: email,
+                            password: '',
+                        },
+                    });
+                } catch (e) { // error on INSERT INTO
+                    res.status(500).json({
+                        success: false,
+                        message: e.message,
+                    });
+                }
+            }
+        } catch (err) { // error on SELECT email
+            res.status(500).json({
+                success: false,
+                message: err.message,
+            });
+        }
+    }
+});
+
+userRouter.post("/login", async (req, res) => {});
 
 userRouter.get("/logout", async (req, res) => {});
 
