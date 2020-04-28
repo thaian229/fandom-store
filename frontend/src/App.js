c2f8f4317e467cf795c38d601c2000859d090814
Nguyễn Thái An <thaian229@gmail.com>
2020-04-28 12:21:52 +0700

    Initialize frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <div></div>
+    );
+  };
+}
+
+export default App;
